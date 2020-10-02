f26c94a046486ab84a73340db85054a5c67de22b
mandursimon <45228040+mandursimon@users.noreply.github.com>
2020-10-02 16:54:29 +0200

    BETA1

--- building_access/settings.py
+++ building_access/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
 
-    
+
+
 ]
 
 MIDDLEWARE = [
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'building_access.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'files/example.db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
